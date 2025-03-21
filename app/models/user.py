8d3c54412a899b5f66f7352b52493fc95ee1d6b9
XSpiritWizardX <dustinboveemusic@gmail.com>
2025-03-20 23:13:31 -0400

    updated user models and seeder

--- app/models/user.py
+++ app/models/user.py
@@ -10,7 +10,9 @@ class User(db.Model, UserMixin):
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(40), nullable=False, unique=True)
+    firstname = db.Column(db.String(40), nullable=False)
+    lastname = db.Column(db.String(40), nullable=False)
+    # username = db.Column(db.String(40), nullable=False, unique=True)
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
 
@@ -28,6 +30,9 @@ class User(db.Model, UserMixin):
     def to_dict(self):
         return {
             'id': self.id,
-            'username': self.username,
+            'firstname': self.firstname,
+            'lastname': self.lastname,
+            # 'username': self.username,
             'email': self.email
+
         }
